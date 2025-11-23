c69585fac091ad846a77a129948cd78ce04a7919
Miguel Gastelumendi <miguelGastelumendi@hotmail.com>
2025-11-23 15:19:29 -0500

    2025-11-23 adjust Linux launcher script rema.sh

--- rema.sh
+++ rema.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Run Canoa
 # mgd
-# Equipe da Canoa -- 2024
+# Equipe da Canoa -- 2024,2025
 # call from miguel@CanoaVM:~/desenv/canoa/carranca$../rema.sh
 source ../../canoa_env_vars.txt
 source ../.venv/bin/activate
 export CANOA_APP_MODE="Production"
 export FLASK_RUN_PORT="5001"
+export FLASK_USE_RELOADER=0
 flask run
+#eof

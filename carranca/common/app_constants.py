44117bc2793466e3dd3d6ca0da13ac3715fb6fa8
miguelGastelumendi <miguelGastelumendi@hotmail.com>
2026-07-28 21:46:58 -0300

    chore: bump to β 5.94

--- carranca/common/app_constants.py
+++ carranca/common/app_constants.py
@@ -16,7 +16,7 @@ app_constants.py
 APP_NAME = "Canoa"
 
 # &beta; major.minor
-APP_VERSION = "β 5.93"  # 2026-07-25
+APP_VERSION = "β 5.94"  # 2026-07-28
 
 # default user HTML/DB lang/locale (see table users.lang)
 APP_LANG = "pt-br"
